0509fe2489bcea6a23b959b7395376c41edc0f44
DiaboloKiat <DiaboloKiat@gmail.com>
2021-07-30 15:24:55 +0800

    nano docker on project_hand

--- docker/trt_hand_pose/nano/docker_run.sh
+++ docker/trt_hand_pose/nano/docker_run.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+   else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+
+#Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+   exit 1
+fi
 
 docker run -it \
-  -e DISPLAY \
-  -e QT_X11_NO_MITSHM=1 \
+  -e DISPLAY=$DISPLAY \
   -e XAUTHORITY=$XAUTH \
   -v "$XAUTH:$XAUTH" \
   -v "/home/$USER/project_hand:/home/diabolokiat/project_hand" \
@@ -12,11 +34,11 @@ docker run -it \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
   -v "/home/$USER/.bashrc:/home/diabolokiat/.bashrc" \
   --workdir "/home/diabolokiat/project_hand" \
-  --name project_hand \
+  --user "root:root" \
+  --name diabolokiat \
+  --runtime nvidia \
   --network host \
   --rm \
-  $DOCKER_OPTS \
   --privileged \
-  --security-opt seccomp=unconfined \
   diabolokiat/project_trt_hand_pose:nano \
   bash
